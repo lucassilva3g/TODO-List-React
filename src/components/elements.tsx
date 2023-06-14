272fbedf488fcd22d70410f125d0e7b5fcc19b80
Lucas <lucassantos.santos90@hotmail.com>
2023-06-14 14:48:21 -0300

    elements

---
+++ src/components/elements.tsx
@@ -0,0 +1,20 @@
+
+
+const Logo = () => {
+  return (
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu-9J30x4uoS8VJDYOcpc4EaYB0Rn06qxRJCErf-1KNccp_ZYXUdNHlzS32zzbb3eRdio&usqp=CAU" alt="" />
+  );
+  }
+  const Input = () => {
+    return (
+      <input type="text" placeholder="Adicione uma nova tarefa" />
+    );
+  }
+
+  const Button = () => {
+    return (
+      <button>Criar + </button>
+    );
+  }
+
+  export { Logo, Input, Button };
