774be533012d3bbd665a24414aab25be773852fa
Jean Oliveira Miranda <jean.miranda@iteris.com.br>
2023-06-22 11:29:22 -0300

    identando

--- frontend/src/components/NoItem/NoItem.tsx
+++ frontend/src/components/NoItem/NoItem.tsx
@@ -1,13 +1,13 @@
-import {Clipboard} from "@phosphor-icons/react";
-import styles from './NoItem.module.css'
-
-
+import { Clipboard } from "@phosphor-icons/react";
+import styles from "./NoItem.module.css";
 
 const NoItem = () => {
   if (NoItem.length === 0) {
     return (
       <div className={styles.itensLista}>
-        <p><Clipboard size={40} /></p>
+        <p>
+          <Clipboard size={40} />
+        </p>
         <p>Você ainda não tem tarefas cadastradas</p>
         <p>Crie tarefas e organize seus itens a fazer</p>
       </div>
