d59aae3315573bcbd8849c4c81dce013bb06d939
Jean Oliveira Miranda <jean.miranda@iteris.com.br>
2023-06-23 12:18:37 -0300

    Fix tests

---
+++ backend/tests/TODO.Service.UnitTest/_Builders/Entities/TodoItemBuilder.cs
@@ -0,0 +1,33 @@
+﻿namespace Todo.Service.UnitTest._Builders.Entities;
+
+public class TodoItemBuilder : BaseComplexDbEntityBuilder<TodoItemBuilder, TodoItem>
+{
+    private bool Active;
+    private bool IsDone;
+
+
+    public static new TodoItemBuilder New()
+    {
+        return new TodoItemBuilder()
+        {
+            Active = true,
+            IsDone = false
+        };
+    }
+
+    protected override TodoItemBuilder GetThis()
+    {
+        return this;
+    }
+
+    public override TodoItem Build()
+    {
+        var model = new TodoItem()
+        {
+            Active = Active,
+            IsDone = IsDone,
+        };
+
+        return base.Build(model);
+    }
+}
