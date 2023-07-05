7bd86a13bd091aced3c3e43e09a914bdfa4954b2
Lucas <116681825+lucassilva3g@users.noreply.github.com>
2023-07-05 14:00:49 -0300

    Feature/tarefas concluidas (#7)
    * checkbox e sublinhado das tasks concluidas
    
    * checkbox e tasks concluidas

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,18 +1,18 @@
-import { Circle, Trash } from "@phosphor-icons/react";
+import { Trash } from "@phosphor-icons/react";
 import styles from "./Item.module.css";
 
 interface ItemProps {
   todo: string;
   done: boolean;
   onDelete: () => void;
+  onDone: () => void;
 }
 
-const Item = ({ todo, done, onDelete }: ItemProps) => {
+const Item = ({ todo, done, onDelete, onDone }: ItemProps) => {
   return (
     <div className={styles.item}>
-      <Circle className={styles.circle} size={17} weight="fill" />
+      <input type="checkbox" onClick={onDone} />
       <span className={done ? styles.done : ""}>{todo}</span>
-
       <Trash className={styles.trash} size={20} onClick={onDelete} />
     </div>
   );
